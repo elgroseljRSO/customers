e5d5ffb881ff50767810ca25f264578b1ed1dae9
elgroselj <ema.ljubljana@gmail.com>
2023-12-10 15:26:54 +0100

    Update init.sql

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,4 +1,3 @@
-INSERT INTO image_metadata (title, description, width, height, created, uri) VALUES ('Our first image', 'This is the first image that we created for the RSO course.', 1280, 1325, TIMESTAMP '2006-01-01 15:36:38', 'https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/tess_first_light-tb.jpg');
 INSERT INTO service_types (name, durationHours, cost) VALUES ('Masaza', 1, 100);
 INSERT INTO employees (name) VALUES ('Janez');
 # INSERT INTO appointments (customer, start, employee_id) VALUES ("stranka@gmail.com", 8, 1);
