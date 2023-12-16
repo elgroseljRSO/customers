fcdd8f79317297ff5e123b310677e2d62fbbfcab
elgroselj <ema.ljubljana@gmail.com>
2023-12-16 11:04:12 +0100

    Remove appointments, employees, serviceTypes

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,9 +1,3 @@
-INSERT INTO service_types (name, durationHours, cost) VALUES ('Masaza', 1, 100);
-INSERT INTO employees (name) VALUES ('Janez');
-# INSERT INTO appointments (customer, start, employee_id) VALUES ("stranka@gmail.com", 8, 1);
-INSERT INTO appointments (start, employee_id, service_type_id, customer) VALUES (8, 1, 1, 'stranka1@gmail.com');
-# INSERT INTO appointments (start, employee_id, customer) VALUES (8, 1, "aba");
-INSERT INTO employees_service_types (service_type_id, employee_id) VALUES (1,1);
 INSERT INTO customers (email, money) VALUES ('stranka1@gmail.com', 100);
 
 
