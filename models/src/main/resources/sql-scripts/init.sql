cea8d832c1a3b23097d556fc93f6ef3a8c2ee838
elgroselj <ema.ljubljana@gmail.com>
2024-01-04 10:09:30 +0100

    Fixup openapi, add to sql

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,3 +1,9 @@
 INSERT INTO customers (email, money) VALUES ('stranka1@gmail.com', 100);
+INSERT INTO customers (email, money) VALUES ('stranka2@gmail.com', 100);
+INSERT INTO customers (email, money) VALUES ('stranka3@gmail.com', 100);
+INSERT INTO customers (email, money) VALUES ('stranka4@gmail.com', 100);
+INSERT INTO customers (email, money) VALUES ('stranka5@gmail.com', 100);
+INSERT INTO customers (email, money) VALUES ('stranka6@gmail.com', 100);
+
 
 
